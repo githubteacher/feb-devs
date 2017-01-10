400e670e7a3bc1e7c5b2ecfc6e26d136e7a9d7fe
BharadwajJannu <BharadwajJannu@users.noreply.github.com>
2017-01-10 18:00:49 +0530

    Add files via upload
    Jenkins

---
+++ BJ/JenkinsTestPanel/JenkinsTestPanel/src/main/java/com/td/iadev/jira/tabpanels/TestIssueTabPanel.java
@@ -0,0 +1,27 @@
+package com.td.iadev.jira.tabpanels;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel;
+import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel;
+//import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
+import com.atlassian.jira.issue.Issue;
+//import com.opensymphony.user.User;
+import java.util.Collections;
+import java.util.List;
+import com.atlassian.crowd.embedded.api.User;
+public class TestIssueTabPanel extends AbstractIssueTabPanel implements IssueTabPanel
+{
+    private static final Logger log = LoggerFactory.getLogger(TestIssueTabPanel.class);
+    
+   
+    public List getActions(Issue issue, User remoteUser) {
+    	//return Collections.singletonList(new GenericMessageAction("This is a message brought to you by the Test Issue Tab Panel"));
+        return Collections.singletonList(new ITAction("This is a message brought to you by the Test Issue Tab Panel", super.descriptor));
+    }
+
+    public boolean showPanel(Issue issue, User remoteUser)
+    {
+        return true;
+    }
+}
