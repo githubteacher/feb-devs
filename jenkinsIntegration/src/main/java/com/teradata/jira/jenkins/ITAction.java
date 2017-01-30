56f016ff16562bb12f843ea817aa68862b443251
BharadwajJannu <BharadwajJannu@users.noreply.github.com>
2017-01-30 16:44:58 +0530

    MavenProj_Github_Jenkins_Integration
    This maven project is added in Github BJ branch. When a commit is done in Github, Jenkins will automatically trigger the build.

---
+++ jenkinsIntegration/src/main/java/com/teradata/jira/jenkins/ITAction.java
@@ -0,0 +1,26 @@
+package com.teradata.jira.jenkins;
+
+import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueAction;
+import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanelModuleDescriptor;
+
+import java.util.Date;
+import java.util.Map;
+
+
+public class ITAction extends AbstractIssueAction {
+
+	String message = "";
+	public ITAction(String msg, IssueTabPanelModuleDescriptor issueTabPanelModuleDescriptor){
+        super(issueTabPanelModuleDescriptor);
+        this.message = msg;
+	}
+
+    public Date getTimePerformed(){
+        return null;
+    }
+
+    public void populateVelocityParams(Map params){
+    	System.out.println("message" + " test control");
+        params.put("ITVal","Issue Tab Panel"+message);
+    }
+}
