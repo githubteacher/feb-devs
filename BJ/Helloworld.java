fd08b67d1c1066857798574dbf99f5139e8a558d
BharadwajJannu <BharadwajJannu@users.noreply.github.com>
2017-02-23 16:58:51 +0530

    Jenkins polling

--- BJ/Helloworld.java
+++ BJ/Helloworld.java
@@ -3,5 +3,6 @@ public class Helloworld{
 	public static void main(String[] args) {
 		System.out.println("Hello Jenkins");
 		System.out.println("Hello!! modifications"); 	
+		System.out.println("Hello!! Jenkins polling");
 	}
 }
