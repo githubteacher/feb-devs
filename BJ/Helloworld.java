dd0f530913b8d9ef4152e33f9d017dc165ea7176
BharadwajJannu <BharadwajJannu@users.noreply.github.com>
2017-02-23 17:22:57 +0530

    Jenkins Git Post receive file

--- BJ/Helloworld.java
+++ BJ/Helloworld.java
@@ -4,5 +4,6 @@ public class Helloworld{
 		System.out.println("Hello Jenkins");
 		System.out.println("Hello!! modifications"); 	
 		System.out.println("Hello!! Jenkins polling");
+		System.out.println("Hello!! Jenkins post receive file");
 	}
 }
